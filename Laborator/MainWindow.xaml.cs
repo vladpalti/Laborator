3c5f6ffcf264e03de14890429a2161d12f8ffe06
Vlad Paltineanu <v.paltineanu@gmail.com>
2021-10-26 13:35:16 +0300

    initial commit

--- Laborator/MainWindow.xaml.cs
+++ Laborator/MainWindow.xaml.cs
@@ -13,54 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Vlad_Paltineanu
+namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DoughnutMachine myDoughnutMachine;
-        private int mRaisedGlazed;
-        private int mRaisedSugar;
-        private int mFilledLemon;
-        private int mFilledChocolate;
-        private int mFilledVanilla;
         public MainWindow()
         {
             InitializeComponent();
-        }
-    }
-    private void frmMain_Loaded(object sender, RoutedEventArgs e)
-    {
-        myDoughnutMachine = new DoughnutMachine();
-        myDoughNutMachine.DoughNutComplete += new DoughnutMachine.DoughnutCompleteDelegate(DoughnutCompleteHandler);
-
-    }
-
-    private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs 0)
-    {
-        glazedToolStripMenuItem.IsChecked = true;
-        sugarToolStripMenuItem.IsChecked = false;
-        myDoughnutMachine.MakeDoughnuts(DoughnutType.Glazed);
-    }
-
-    private void sugarToolStripMenuItem_Click(object sender, RoutedEventArgs)
-    {
-        glazedToolStripMenuItem.IsChecked = false;
-        sugarToolStripMenuItem.IsChecked = true;
-        myDoughnutMachine.MakeDoughnuts(DoughnutType.Sugar);
-    }
-
-    private void DoughnutCompleteHandler()
-    {
-        switch (myDoughnutMachine.Flavor)
-        {
-            case DoughnutType.Glazed:
-                mRaisedGlazed++;
-                txt.GlazedRaised.Text = mRaisedGlazed.ToString();
-                break;
-
         }
     }
 }
